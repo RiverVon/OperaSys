a8163c9e88f570c00f712ac419e9924e967ef082
YuanFeng <yuanfeng7710@gmail.com>
2025-11-06 16:23:12 +0800

    实验二新增

---
+++ my_project/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main() {
+    printf("Hello, World!\n");
+    greet("Student");
+    return 0;
+}
---
+++ my_project/src/utils.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "utils.h"
+
+void greet(const char *name) {
+    printf("Welcome, %s!\n", name);
+}
